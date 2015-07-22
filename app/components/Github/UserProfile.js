6ee88745f60ef74e85fe18737d6bb7909c444a57
Jon Roby <jonathan.roby1@gmail.com>
2015-07-22 14:34:09 -0700

    Create Repos, UserProfile and Notes components

---
+++ app/components/Github/UserProfile.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var UserProfiles = React.createClass({
+  render: function() {
+    return (
+      <div> UserProfile </div>
+    )
+  }
+});
+
+module.exports = UserProfiles;
